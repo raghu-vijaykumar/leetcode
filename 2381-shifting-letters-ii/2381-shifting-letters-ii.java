64d859718e3348f00fe0373aab6a7e05547b1c3d
Raghu Vijaykumar <raghu.dinka.vijaykumar@gmail.com>
2025-01-05 20:07:52 +0530

    Time: 10 ms (18.4%), Space: 74.5 MB (56.13%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.java
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.java
@@ -7,13 +7,11 @@ class Solution {
             res[shift[0]] = shift[2] == 1 ? res[shift[0]] - 1 : res[shift[0]] + 1;
         }
 
-        //System.out.println(Arrays.toString(res));
-
-        int diff = (res[res.length - 1] + 26) % 26;
+        int diff = res[res.length - 1];
         StringBuilder sb = new StringBuilder();
         for (int i = res.length - 2; i >= 0; i--) {
-            sb.append((char) ('a' + (s.charAt(i) - 'a' + diff) % 26));
-            diff += (res[i] + 26) % 26;
+            sb.append((char) ('a' + (((s.charAt(i) - 'a' + diff) % 26 + 26) % 26)));
+            diff += res[i];
         }
 
         return sb.reverse().toString();
