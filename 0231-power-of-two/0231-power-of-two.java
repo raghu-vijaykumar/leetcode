c323306214024207b80474129b8cfe6bc71b264b
Raghu Vijaykumar <raghu.dinka.vijaykumar@gmail.com>
2025-01-15 14:53:32 +0530

    Time: 0 ms (100%), Space: 41.2 MB (15.88%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n == 0)
-            return false;
-        for (int i = 0; i < 31; i++) {
-            if ((n | (1 << i)) == (1 << i))
-                return true;
-        }
-        return false;
+        return n > 0 && Integer.bitCount(n) == 1;
     }
 }
