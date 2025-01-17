e54961b5d86eb2c05a9f80fdf48e47759e2239f4
Raghu Vijaykumar <raghu.dinka.vijaykumar@gmail.com>
2025-01-17 13:28:48 +0530

    Time: 1156 ms (5.07%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-SELECT DISTINCT t1.name from salesperson t1  LEFT JOIN orders t2 ON t1.sales_id = t2.sales_id WHERE t1.sales_id NOT IN (SELECT DISTINCT sales_id from orders WHERE com_id = (SELECT com_id from company where name = 'RED'));
+SELECT DISTINCT t1.name from salesperson t1  LEFT JOIN orders t2 ON t1.sales_id = t2.sales_id  WHERE t1.sales_id NOT IN (SELECT DISTINCT sales_id from orders t3 INNER JOIN company t4 ON t3.com_id = t4.com_id AND t4.name = 'RED');
